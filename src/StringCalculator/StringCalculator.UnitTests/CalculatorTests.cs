83d40ceadfa01814f8eadd6a98a2032c0c9531cc
joachim.lovf <joachim.lovf@gmail.com>
2014-06-14 22:08:12 +0200

    Project and test project created, test libraries added and basic structure completed for Calculator tests.

---
+++ src/StringCalculator/StringCalculator.UnitTests/CalculatorTests.cs
@@ -0,0 +1,22 @@
+﻿using Should;
+using NUnit.Framework;
+
+namespace StringCalculator.UnitTests
+{
+    public class CalculatorTests
+    {
+        private Calculator _sut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _sut = new Calculator();
+        }
+
+        [Test]
+        public void An_empty_string_returns_0()
+        {
+            
+        }
+    }
+}
